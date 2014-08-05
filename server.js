3454c0897e68fc3c3af84ecdd94d5c9e4f58dfa5
Francesco Farina <rockerg991@gmail.com>
2014-08-05 11:55:59 +0200

    Fixed empty woeid bug

--- server.js
+++ server.js
@@ -11,22 +11,27 @@ function sendJSON(data, wstream){
 	wstream.end(JSON.stringify(data));
 }
 
-function sendWeather(err, weather){
-	if (err)
-		{sendJSON(errorResponse, res); return;}
 
-	sendJSON(weather, res);
-}
 
 
 http.createServer(function(req, res){
 	if (req.method === "GET"){
 		var parsedURL = url.parse(req.url, true);
 		if (parsedURL.pathname.match("^/weatherapi/city")){
-			weatherlike.inCity(parsedURL.query.city, sendWeather);
+			weatherlike.inCity(parsedURL.query.city, function(err, weather){
+				if (err)
+					{sendJSON(errorResponse, res); return;}
+
+				sendJSON(weather, res);
+			});
 		}
 		else if (parsedURL.pathname.match("^/weatherapi/woeid")){
-			weatherlike.inWoeid(parsedURL.query.woeid, sendWeather);
+			weatherlike.inWoeid(parsedURL.query.woeid, function(err, weather){
+				if (err)
+					{sendJSON(errorResponse, res); return;}
+
+				sendJSON(weather, res);
+			});
 		}
 	}
 }).listen(PORT);
